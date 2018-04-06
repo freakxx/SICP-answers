a1128067173605d03b14fc05f10e7a7a8067ae89
郭奕佳 <gyj@gzqichang.com>
2018-04-06 13:38:50 +0800

    add char 1.1 ex1.6-1.8

---
+++ ch01/ex1.06.rkt
@@ -0,0 +1,8 @@
+#lang sicp
+
+
+#|
+  递归层次太深。
+
+  可以参考： http://sicp.readthedocs.io/en/latest/chp1/6.html
+|#
---
+++ ch01/ex1.07.rkt
@@ -0,0 +1,40 @@
+#lang sicp
+
+#|
+参考： http://sicp.readthedocs.io/en/latest/chp1/7.html
+|#
+
+(define (sqrt-iter guess x)
+  (if (new-good-enough guess (improve guess x))
+      (improve guess x)
+      (sqrt-iter (improve guess x) x)
+      )
+  )
+
+
+(define (good-enough guess x)
+  (< (abs (- (square guess) x)) 0.001)
+  )
+
+(define (improve guess x)
+  (average guess (/ x guess))
+  )
+
+(define (average x y)
+  (/ (+ x y) 2)
+  )
+
+(define (square x)
+  (* x x)
+  )
+
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x)
+  )
+
+
+
+(define (new-good-enough old-guess new-guess)
+  (> 0.01 (/ (abs (- new-guess old-guess)) old-guess))
+  )
