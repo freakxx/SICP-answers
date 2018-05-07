eb4ba1aed80414610e14a19089c700a715e4bb80
郭奕佳 <gyj@gzqichang.com>
2018-05-08 01:46:35 +0800

    add char 1 ex 1.09-1.10

---
+++ ch01/ex1.09.rkt
@@ -0,0 +1,37 @@
+#lang sicp
+
+(define (plus a b)
+  (if (= a 0)
+      b
+      (inc (plus (dec a) b))))
+
+
+(define (plus-new a b)
+  (if (= a 0)
+      b
+      (plus-new (dec a) (inc b))))
+
+
+
+#|
+(plus 4 5)
+(inc (plus 3 5))
+(inc (inc (plus 2 5)))
+(inc (inc (inc (plus 1 5))))
+(inc (inc (inc (inc (plus 0 5)))))
+(inc (inc (inc (inc (5)))))
+(inc (inc (inc 6)))
+(inc (inc 7))
+(inc 8)
+9
+|#
+
+
+#|
+(plus-new 4 5)
+(plus-new 3 6)
+(plus-new 2 7)
+(plus-new 1 8)
+(plus-new 0 9)
+9
+|#
